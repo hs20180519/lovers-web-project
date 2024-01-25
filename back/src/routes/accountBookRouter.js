97874ada202e42e6986336d11bec4ae025c0f8a4
Taeyoung Kim <legends0427@gmail.com>
2024-01-26 00:05:50 +0100

    🔐 Add: Authorization in accountbook api
    가계부 api에 인가 추가

--- back/src/routes/accountBookRouter.js
+++ back/src/routes/accountBookRouter.js
@@ -4,8 +4,16 @@ const { authenticateJwt } = require("../middlewares/passportJwt");
 
 const accountBookRouter = express.Router();
 
-accountBookRouter.post("/posts", accountBookController.createAccountBook);
-accountBookRouter.delete("/posts/:accountBookPostId", accountBookController.deleteAccountBookById);
-accountBookRouter.get("/posts/:lover_id", accountBookController.getAccountBookByDate);
+accountBookRouter.post("/posts", authenticateJwt, accountBookController.createAccountBook);
+accountBookRouter.delete(
+  "/posts/:accountBookPostId",
+  authenticateJwt,
+  accountBookController.deleteAccountBookById,
+);
+accountBookRouter.get(
+  "/posts/:lover_id",
+  authenticateJwt,
+  accountBookController.getAccountBookByDate,
+);
 
 module.exports = accountBookRouter;
