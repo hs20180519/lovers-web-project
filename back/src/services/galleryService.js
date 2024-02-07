ed0fa73657371e69c971d5fbb4ce8e136a97809e
hs20180519 <momo33559@naver.com>
2024-02-07 22:06:38 +0100

    :art: Refactor: GalleryService & GalleryController.js

--- back/src/services/galleryService.js
+++ back/src/services/galleryService.js
@@ -2,39 +2,29 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 class GalleryService {
   async createGalleryPhoto(loverId, imagePath) {
-    try {
-      return await prisma.gallery_photos.create({
-        data: {
-          lover_id: loverId,
-          image_path: imagePath,
-          photo_date: new Date(),
-        },
-      });
-    } catch (error) {
-      throw error;
-    }
+    const photo = await prisma.gallery_photos.create({
+      data: {
+        lover_id: loverId,
+        image_path: imagePath,
+        photo_date: new Date(),
+      },
+    });
+    return photo;
   }
   async getAllGalleryPhotos(loverId) {
-    try {
-      return await prisma.gallery_photos.findMany({
-        where: {
-          lover_id: loverId,
-        },
-      });
-    } catch (error) {
-      throw error;
-    }
+    const photos = await prisma.gallery_photos.findMany({
+      where: {
+        lover_id: loverId,
+      },
+    });
+    return photos;
   }
   async deleteGalleryPhoto(galleryPhotoId) {
-    try {
-      await prisma.gallery_photos.delete({
-        where: {
-          gallery_photo_id: galleryPhotoId,
-        },
-      });
-    } catch (error) {
-      throw error;
-    }
+    await prisma.gallery_photos.delete({
+      where: {
+        gallery_photo_id: galleryPhotoId,
+      },
+    });
   }
 }
 module.exports = new GalleryService();
