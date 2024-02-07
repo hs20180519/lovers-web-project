71ebc1e7335ed76bd61e3b42d00de54681945632
hs20180519 <momo33559@naver.com>
2024-02-07 20:58:56 +0100

    :art: Refactor: DiaryService & DiaryController

--- back/src/controllers/diaryController.js
+++ back/src/controllers/diaryController.js
@@ -1,48 +1,42 @@
-const diaryService = require("../services/diaryService");
-const logger = require("../config/logger");
+const diaryService = require("../services/DiaryService");
 
 class DiaryController {
-  async createDiaryPost(req, res) {
+  async createDiaryPost(req, res, next) {
     const userId = req.user.user_id;
     const { title, content, postDate, loverId } = req.body;
     try {
       const post = await diaryService.createDiaryPost(title, content, loverId, userId, postDate);
       res.status(201).json({ post });
     } catch (error) {
-      logger.error("Error during createPost", error);
-      res.status(500).json({ error: "Internal server error during post creation." });
+      next(error);
     }
   }
-
-  async updateDiaryPost(req, res) {
+  async updateDiaryPost(req, res, next) {
     const { title, content, diaryPostId, postDate } = req.body;
     try {
-      const post = await diaryService.updateDiaryPost(title, content, diaryPostId, postDate);
-      res.status(200).json({ post });
+      const updatedPost = await diaryService.updateDiaryPost(title, content, diaryPostId, postDate);
+      res.status(200).json({ updatedPost });
     } catch (error) {
-      logger.error("Error during updatePost", error);
-      res.status(500).json({ error: "Internal server error during post update." });
+      next(error);
     }
   }
-  async deleteDiaryPost(req, res) {
+  async deleteDiaryPost(req, res, next) {
     const diaryPostId = parseInt(req.params.diary_post_id);
     try {
       await diaryService.deleteDiaryPost(diaryPostId);
       res.status(204).send();
     } catch (error) {
-      logger.error("Error during deletePost", error);
-      res.status(500).json({ error: "Internal server error during post delete." });
+      next(error);
     }
   }
-  async getMonthlyDiaryPosts(req, res) {
+  async getMonthlyDiaryPosts(req, res, next) {
     const loverId = parseInt(req.params.lover_id);
     const { year, month } = req.query;
     try {
       const posts = await diaryService.getMonthlyDiaryPosts(loverId, year, month);
       res.status(200).json({ posts });
     } catch (error) {
-      logger.error("Error during getPosts", error);
-      res.status(500).json({ error: "Internal server error during posts get." });
+      next(error);
     }
   }
 }
